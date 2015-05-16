521e03d49f21e7269dae5801e3f8bebab48c33be
Kristoffer Dalby <kradalby@kradalby.no>
2015-05-16 19:06:35 +0200

    added some api endpoints, testing

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var app      = express()
 
 var router = express.Router()
 
+var JSON_SUCCESS = {"status": "success"}
+
 router.route('/servers')
     .get(function (req, res) {
         srv.serverlist(function (err, data) {
@@ -14,9 +16,38 @@ router.route('/servers')
         })
     })
 
-router.route('/servers/:server')
-    .get(function () {
-        
+router.route('/servers/start')
+    .get(function (req, res) {
+        srv.serverlist(function(err, data) {
+            if (err) {
+                res.send(err)
+            }
+            srv.start(data)
+            res.json(JSON_SUCCESS)
+        })
+    })
+
+router.route('/servers/stop')
+    .get(function (req, res) {
+        srv.serverlist(function(err, data) {
+            if (err) {
+                res.send(err)
+            }
+            srv.stop(data)
+            res.json(JSON_SUCCESS)
+        })
+    })
+
+router.route('/servers/:server/start')
+    .get(function (req, res) {
+        srv.start([req.params.server])
+        res.json(JSON_SUCCESS)
+    })
+
+router.route('/servers/:server/stop')
+    .get(function (req, res) {
+        srv.stop([req.params.server])
+        res.json(JSON_SUCCESS)
     })
 
 app.use('/api/v1', router)
